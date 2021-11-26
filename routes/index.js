df3fbe3d498c6f12be6c27d3f2e826c14d81c81a
김동호 <kimdongho@gimdonghoui-MacBookPro.local>
2021-11-26 15:32:59 +0900

    Setting Vue

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/getTest', async (req, res) => {
+  try {
+    res.send({
+      msg: 'This my response : get'
+    })
+  } catch (err) {
+    console.log(err);
+    res.send({
+      error: 'Can"t read api data',
+    });
+  }
+});
+
+router.post('/api/postTest', async (req, res) => {
+  try {
+    console.log(req.body)
+    res.send({
+      msg: 'Hi!' + req.body.name
+    })
+  } catch (err) {
+    console.log(err);
+    res.send({
+      error: 'Can"t read api data',
+    });
+  }
+});
+
+
 module.exports = router;
